c3dad2715786c9257f4f908de6b1f0d711bef3f5
KimonoMyHouse <mehdinadif@hotmail.com>
2015-04-26 19:18:36 +0200

    Added the large precise test, along with a few plots

--- KmerTesting/src/MinHashMain.java
+++ KmerTesting/src/MinHashMain.java
@@ -10,7 +10,7 @@ public class MinHashMain {
 		HashManager hm = new HashManager();
 		IOManager io = new IOManager();
 		long then = System.nanoTime();
-		/*
+		/* FOR TESTING THE LARGE PICTURE OF KMER AND HASHSIZE
 		for(int n = 1; n < 6; n++){
 			
 			String filein = "C:/Users/User/Documents/KU/Attending/BachelorProjekt/KmerTesting/testData/test" + n + ".fna";
@@ -52,6 +52,7 @@ public class MinHashMain {
 		double seconds =(double) taken / 1000000000.0; 
 		System.out.println("The time it took in seconds was " + seconds);
 		*/
+		/*
 		for(int n = 1; n < 6; n++){
 			
 			String filein = "C:/Users/User/Documents/KU/Attending/BachelorProjekt/KmerTesting/testData/test" + n + ".fna";
@@ -79,6 +80,88 @@ public class MinHashMain {
 				System.out.println(n + " " + i);
 				ic.KMER_SIZE = i;
 				for (int j : hashTestSizes) {
+					hm.HASH_SIZE = j;
+					MinHash minHash = new MinHash(mg, ic, hm);
+				}
+			}
+			ic.pw.close();
+		}
+		long now = System.nanoTime();
+		
+		long taken = now - then;
+
+		double seconds =(double) taken / 1000000000.0; 
+		System.out.println("The time it took in seconds was " + seconds);
+		*/
+		// for Testing at k=10
+		/*
+		for(int n = 1; n < 6; n++){
+			
+			String filein = "C:/Users/User/Documents/KU/Attending/BachelorProjekt/KmerTesting/testData/test" + n + ".fna";
+			String fileout = "C:/Users/User/Documents/KU/Attending/BachelorProjekt/KmerTesting/testData/test" + n + "outprecise10.fna";
+
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fileout)));
+			
+			ic.inputfileString = filein;
+			ic.pw = pw;
+			
+	        ic.threshold = 0.95;
+	        ic.program = "dna";
+	
+			StringMapper strmap = new StringMapper(ic.program);
+			mg.BASES_LENGTH = 4;
+	        hm.prime_div = 1845587707;
+	
+			io.ReadMetagenomeData(mg, strmap, ic);
+	
+			
+			int[] kmerTestSizes = {10};
+	
+			for (int i : kmerTestSizes) {
+				System.out.println(n + " " + i);
+				ic.KMER_SIZE = i;
+				for (int j =15;j<40;j++) {
+					hm.HASH_SIZE = j;
+					MinHash minHash = new MinHash(mg, ic, hm);
+				}
+			}
+			ic.pw.close();
+		}
+		long now = System.nanoTime();
+		
+		long taken = now - then;
+
+		double seconds =(double) taken / 1000000000.0; 
+		System.out.println("The time it took in seconds was " + seconds);
+		
+		*/
+		// The big one
+		for(int n = 1; n < 2; n++){
+			
+			String filein = "C:/Users/User/Documents/KU/Attending/BachelorProjekt/KmerTesting/testData/test" + n + ".fna";
+			String fileout = "C:/Users/User/Documents/KU/Attending/BachelorProjekt/KmerTesting/testData/test" + n + "outpreciselarge.fna";
+
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fileout)));
+			
+			ic.inputfileString = filein;
+			ic.pw = pw;
+			
+	        ic.threshold = 0.95;
+	        ic.program = "dna";
+	        
+	
+			StringMapper strmap = new StringMapper(ic.program);
+			mg.BASES_LENGTH = 4;
+	        hm.prime_div = 1845587707;
+	
+			io.ReadMetagenomeData(mg, strmap, ic);
+	
+			
+	
+			for (int i = 5; i< 11; i++) {
+				System.out.println(n + " " + i);
+				ic.KMER_SIZE = i;
+				for (int j =15;j<70;j++) {
 					hm.HASH_SIZE = j;
 					MinHash minHash = new MinHash(mg, ic, hm);
 				}
