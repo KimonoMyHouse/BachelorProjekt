37201baad4f91ee13c51170e188f73879b1258b6
KimonoMyHouse <mehdinadif@hotmail.com>
2015-04-26 17:38:24 +0200

    Added the tests that perform for k=5 and h=60-80, which are more precise

--- KmerTesting/src/MinHashMain.java
+++ KmerTesting/src/MinHashMain.java
@@ -10,7 +10,7 @@ public class MinHashMain {
 		HashManager hm = new HashManager();
 		IOManager io = new IOManager();
 		long then = System.nanoTime();
-		
+		/*
 		for(int n = 1; n < 6; n++){
 			
 			String filein = "C:/Users/User/Documents/KU/Attending/BachelorProjekt/KmerTesting/testData/test" + n + ".fna";
@@ -34,6 +34,46 @@ public class MinHashMain {
 			
 			int[] kmerTestSizes = {2,4,5,7,10,12,15,20,25,30};
 			int[] hashTestSizes = {25,40,50,70,100,150,200,400};
+	
+			for (int i : kmerTestSizes) {
+				System.out.println(n + " " + i);
+				ic.KMER_SIZE = i;
+				for (int j : hashTestSizes) {
+					hm.HASH_SIZE = j;
+					MinHash minHash = new MinHash(mg, ic, hm);
+				}
+			}
+			ic.pw.close();
+		}
+		long now = System.nanoTime();
+		
+		long taken = now - then;
+
+		double seconds =(double) taken / 1000000000.0; 
+		System.out.println("The time it took in seconds was " + seconds);
+		*/
+		for(int n = 1; n < 6; n++){
+			
+			String filein = "C:/Users/User/Documents/KU/Attending/BachelorProjekt/KmerTesting/testData/test" + n + ".fna";
+			String fileout = "C:/Users/User/Documents/KU/Attending/BachelorProjekt/KmerTesting/testData/test" + n + "outprecise.fna";
+
+			PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(fileout)));
+			
+			ic.inputfileString = filein;
+			ic.pw = pw;
+			
+	        ic.threshold = 0.95;
+	        ic.program = "dna";
+	
+			StringMapper strmap = new StringMapper(ic.program);
+			mg.BASES_LENGTH = 4;
+	        hm.prime_div = 1845587707;
+	
+			io.ReadMetagenomeData(mg, strmap, ic);
+	
+			
+			int[] kmerTestSizes = {5};
+			int[] hashTestSizes = {60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78};
 	
 			for (int i : kmerTestSizes) {
 				System.out.println(n + " " + i);
